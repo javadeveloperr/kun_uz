fd9c2e4a19d33219e36b4ce5c5c999ac40b91599
javadeveloperr <ergashevdilshod807@gmail.com>
2023-04-20 00:43:46 +0500

    Initial commit

---
+++ src/main/java/com/example/service/ProfileService.java
@@ -0,0 +1,54 @@
+package com.example.service;
+
+import com.example.dto.ProfileDTO;
+import com.example.entity.ProfileEntity;
+import com.example.enums.GeneralStatus;
+import com.example.enums.ProfileRole;
+import com.example.exps.AppBadRequestException;
+import com.example.exps.MethodNotAllowedException;
+import com.example.repository.ProfileRepository;
+import com.example.util.MD5Util;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class ProfileService {
+
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    public ProfileDTO create(ProfileDTO dto, Integer adminId) {
+        // check - homework
+        isValidProfile(dto);
+
+        ProfileEntity entity = new ProfileEntity();
+        entity.setName(dto.getName());
+        entity.setSurname(dto.getSurname());
+        entity.setPhone(dto.getPhone());
+        entity.setEmail(dto.getEmail());
+        entity.setRole(dto.getRole());
+        entity.setPassword(MD5Util.getMd5Hash(dto.getPassword())); // MD5 ?
+        entity.setCreatedDate(LocalDateTime.now());
+        entity.setVisible(true);
+        entity.setStatus(GeneralStatus.ACTIVE);
+        entity.setPrtId(adminId);
+        profileRepository.save(entity); // save profile
+
+        dto.setPassword(null);
+        dto.setId(entity.getId());
+        return dto;
+    }
+
+    public void isValidProfile(ProfileDTO dto) {
+        // throw ...
+    }
+
+    public ProfileEntity get(Integer id) {
+        return profileRepository.findById(id).orElseThrow(() -> {
+            throw new AppBadRequestException("Profile not found");
+        });
+    }
+
+}
